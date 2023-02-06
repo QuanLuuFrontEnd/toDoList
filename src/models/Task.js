443e2779125620c1911e34b7d62ca96f8a085882
quan luu <hoangquan67298312@gmail.com>
2023-02-06 10:31:17 +0700

    first commit

---
+++ src/models/Task.js
@@ -0,0 +1,6 @@
+function Task(_id, _taskName, _status) {
+    // properties
+    this.id = _id;
+    this.taskName = _taskName;
+    this.status = _status;
+}
---
+++ src/models/TaskList.js
@@ -0,0 +1,8 @@
+function TaskList(){
+    this.arr=[];
+    // _findIndex (tìm vị trí theo id)
+    // ➢ addTask,
+    // ➢ deleteTask,
+    // ➢ getTaskById (lấy thông tin task: gồm gồm các thuộc tính),
+    // ➢ updateTask.
+}
